31a91e387ac41fe17fd34ea86fe8c0a44d43e0c3
Martin Gross <martin@pc-coholic.de>
2014-11-12 18:51:58 -0500

    Adding Logout-Butotn

--- index.php
+++ index.php
@@ -46,13 +46,9 @@ check_auth();
           <a class="navbar-brand" href="#">IFT2935-A14-P7</a>
         </div>
         <div id="navbar" class="collapse navbar-collapse">
-<!--
           <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
+              <li class="active"><a href="https://identification.umontreal.ca/cas/logout.ashx">Logout</a></li>
           </ul>
--->
         </div><!--/.nav-collapse -->
       </div>
     </nav>
