4f66096ee8909877453ad84289722cd7a100fc86
Martin Gross <martin@pc-coholic.de>
2014-11-12 18:30:03 -0500

    missing .js-file

--- index.php
+++ index.php
@@ -99,5 +99,6 @@ check_auth();
     <script src="/js/bootstrap.min.js"></script>
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <script src="/js/ie10-viewport-bug-workaround.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
   </body>
 </html>
