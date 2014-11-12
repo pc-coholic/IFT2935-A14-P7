2bcf028a24ea90e3b9d4d6df56d755ff89467917
Martin Gross <martin@pc-coholic.de>
2014-11-12 17:57:58 -0500

    moar dumping

--- auth.php
+++ auth.php
@@ -15,8 +15,11 @@ function do_udem_auth() {
 }
 
 function check_udem_auth() {
-  $authresponse = simplexml_load_file("https://identification.umontreal.ca/cas/serviceValidate.ashx?ticket=" . $_GET['ticket'] . "&service=" . BASEURL);
+  $authurl = "https://identification.umontreal.ca/cas/serviceValidate.ashx?ticket=" . $_GET['ticket'] . "&service=" . BASEURL;
+  $authresponse = simplexml_load_file($authurl);
+  print_r($auth_url);
   print_r($authresponse);
+  var_dump($authresponse);
 }
 
 ?>
