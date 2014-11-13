e5df7f04dc29f4418416ed89c55365293f293bf7
Martin Gross <martin@pc-coholic.de>
2014-11-12 19:35:18 -0500

    remove active-tag from logout-butotn

--- index.php
+++ index.php
@@ -47,7 +47,7 @@ check_auth();
         </div>
         <div id="navbar" class="collapse navbar-collapse">
           <ul class="nav navbar-nav navbar-right">
-              <li class="active"><a href="https://identification.umontreal.ca/cas/logout.ashx">Logout <?= $_SESSION['user'][0] ?></a></li>
+              <li><a href="https://identification.umontreal.ca/cas/logout.ashx">Logout <?= $_SESSION['user'][0] ?></a></li>
           </ul>
         </div><!--/.nav-collapse -->
       </div>
