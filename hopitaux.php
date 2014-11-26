dfbd53eb25ec625ef29a9259b8d1298b5d7013b6
Martin Gross <martin@pc-coholic.de>
2014-11-26 16:29:48 -0500

    DB debug

--- hopitaux.php
+++ hopitaux.php
@@ -1,8 +1,8 @@
 <?php
 $pdo = new PDO(getenv('DB_DSN'), getenv('DB_USER'), getenv('DB_PASS'));
 
-$array = $pdo->query("SELECT * FROM hopital")->fetchAll(PDO::FETCH_ASSOC);
+$array = $pdo->query("SELECT * FROM hopital", $link) or die(mysql_error($link));
+$array->fetchAll(PDO::FETCH_ASSOC);
 print json_encode($array);
-print "foobar";
 $pdo = null;
 ?>
