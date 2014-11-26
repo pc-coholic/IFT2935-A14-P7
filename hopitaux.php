0699768198d6dbab1c6f9d9a607bcf193f377714
Martin Gross <martin@pc-coholic.de>
2014-11-26 17:06:54 -0500

    Fucking wrong variable

--- hopitaux.php
+++ hopitaux.php
@@ -1,16 +1,13 @@
 <?php
-$con=mysqli_connect(getenv('DB_HOST'), getenv('DB_USER'), getenv('DB_PASS'), getenv('DB_BASE'));
+$con=mysqli_connect(getenv('DB_HOST'), getenv('DB_USER'), getenv('DB_PASS'), getenv('DB_DB'));
 
 // Check connection
-if (mysqli_connect_errno())
-  {
+if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
+}
 
 // Perform queries 
 $sth = mysqli_query($con, "SELECT * FROM hopital");
-
-echo("Error description: " . mysqli_error($con));
 
 $rows = array();
 while($r = mysqli_fetch_assoc($sth)) {
