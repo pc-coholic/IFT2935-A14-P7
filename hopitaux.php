aa7bb671fc752d07adb2fd303c92265222ebedc7
Martin Gross <martin@pc-coholic.de>
2014-11-26 17:18:27 -0500

    moar debug

--- hopitaux.php
+++ hopitaux.php
@@ -17,22 +17,7 @@ while($r = mysqli_fetch_assoc($sth)) {
 
 $json = json_encode($rows);
 
-switch(json_last_error())
- {
-  case JSON_ERROR_DEPTH:
-   print ' - Maximale Stacktiefe überschritten';
-  break;
-  case JSON_ERROR_CTRL_CHAR:
-   print ' - Unerwartetes Steuerzeichen gefunden';
-  break;
-  case JSON_ERROR_SYNTAX:
-   print ' - Syntaxfehler, ungültiges JSON';
-  break;
-  case JSON_ERROR_NONE:
-   print ' - Keine Fehler';
-  break;
- }
-
+print json_last_error();
 print $json;
 var_dump($json);
 
