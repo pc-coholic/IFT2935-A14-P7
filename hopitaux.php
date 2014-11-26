c0b2d175cea7d24991026a6c3cf3260e43fa5507
Martin Gross <martin@pc-coholic.de>
2014-11-26 16:25:01 -0500

    Convert to new connection-string

--- hopitaux.php
+++ hopitaux.php
@@ -1,5 +1,5 @@
 <?php
-$pdo = new PDO(getenv('CLEARDB_DATABASE_URL'));
+$pdo = new PDO(getenv('DB_DSN'), getenv('DB_USER'), getenv('DB_PASS'));
 
 $array = $pdo->query("SELECT * FROM hopital")->fetchAll(PDO::FETCH_ASSOC);
 echo json_encode($array);
