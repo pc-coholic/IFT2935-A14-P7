a5faae08af4f05c09af97890d6c445767a92fae3
Martin Gross <martin@pc-coholic.de>
2014-11-30 15:26:24 -0500

    Adding GPS-Locator-Button to modal

--- index.php
+++ index.php
@@ -112,11 +112,6 @@ check_auth();
                 }, "LocateButton");
                 geoLocate.startup();
 
-                geoLocate2 = new LocateButton({
-                  map: map
-                }, "locategps");
-                geoLocate2.startup();
-
                 $.getJSON( "hopitaux.php", function( data ) {
                   $.each( data, function( key, val ) {
                     //alert(val['Nom']);
@@ -134,12 +129,12 @@ check_auth();
 
                locator = new Locator("https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer");
                locator.on("address-to-locations-complete", showResults);
-/*
+
                $("locategps").click( function() {
-                 geoLocate.startup();
+                 geoLocate._locate();
                  $('#shortModal').modal('hide');
                });
-*/
+
                $("#locateform").submit( function() {
                  event.preventDefault();
                  var address = {
