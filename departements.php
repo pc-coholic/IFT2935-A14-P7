f38833bc61afd3f441a914d3cd6bee43238512a0
Martin Gross <martin@pc-coholic.de>
2014-11-30 16:36:51 -0500

    Change dept-query

--- departements.php
+++ departements.php
@@ -1,5 +1,5 @@
 <?php
 require("db.php");
 
-print query_json("SELECT * FROM departement WHERE ID = " . $_GET['ID']);
+print query_json("SELECT dept_hopital.ID, Nom FROM dept_hopital LEFT JOIN departement ON ID_D = departement.ID WHERE ID_H = " . $_GET['ID']);
 ?>
