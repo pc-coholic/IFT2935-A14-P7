90db3f16f372835b68f647db2b86c133523687a3
Martin Gross <martin@pc-coholic.de>
2014-12-03 18:26:23 -0500

    Notifaction-handler

--- notification.php
+++ notification.php
@@ -1,3 +1,5 @@
 <?php
-var_dump($_POST);
+require("db.php");
+
+print query_json("UPDATE patient SET TypeEnvoi = '" . $_POST['selectModeEnvoie3'] . "', min_patient = '" . $_POST['selectNoPatients3'] . "' WHERE numeroPatient = " . $_POST['inputNoPatient3']);
 ?>
