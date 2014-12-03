15f5147efb19916a402329c0f4feb8ac207b3667
Martin Gross <martin@pc-coholic.de>
2014-12-03 17:51:29 -0500

    change notify form to real form

--- index.php
+++ index.php
@@ -97,29 +97,50 @@ check_auth();
           <div class="modal-body">
             <form class="form-horizontal" role="form">
               <div class="form-group">
-                <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+                <label for="inputNoPatient3" class="col-sm-2 control-label">Numero patient</label>
                 <div class="col-sm-10">
-                  <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                  <input type="number" class="form-control" id="inputNoPatient3" placeholder="Numero patient">
                 </div>
               </div>
               <div class="form-group">
-                <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+                <label for="selectModeEnvoie3" class="col-sm-2 control-label">Mode d'envoie</label>
                 <div class="col-sm-10">
-                  <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                  <select class="form-control" id="selectModeEnvoie3">
+                    <option>tel</option>
+                    <option>email</option>
+                  </select>
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="selectNoPatients3" class="col-sm-2 control-label">Nombre de patients</label>
+                <div class="col-sm-10">
+                  <select class="form-control" id="selectNoPatients3">
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option selected="selected">5</option>
+                    <option>6</option>
+                    <option>7</option>
+                    <option>8</option>
+                    <option>9</option>
+                    <option>10</option>
+                    <option>11</option>
+                    <option>12</option>
+                    <option>13</option>
+                    <option>14</option>
+                    <option>15</option>
+                    <option>16</option>
+                    <option>17</option>
+                    <option>18</option>
+                    <option>19</option>
+                    <option>20</option>
+                  </select>
                 </div>
               </div>
               <div class="form-group">
                 <div class="col-sm-offset-2 col-sm-10">
-                  <div class="checkbox">
-                    <label>
-                      <input type="checkbox"> Remember me
-                    </label>
-                  </div>
-                </div>
-              </div>
-              <div class="form-group">
-                <div class="col-sm-offset-2 col-sm-10">
-                  <button type="submit" class="btn btn-default">Sign in</button>
+                  <button type="submit" class="btn btn-default">Enregistrer</button>
                 </div>
               </div>
             </form>
