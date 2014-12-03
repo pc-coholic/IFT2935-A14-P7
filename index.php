dd1557274ff82215c8aada7872025fd6828347c7
Martin Gross <martin@pc-coholic.de>
2014-12-03 17:29:51 -0500

    Retabbing

--- index.php
+++ index.php
@@ -64,15 +64,15 @@ check_auth();
       </div>
     </div><!-- /.container -->
 
-      <div id="shortModal" class="modal modal-wide fade">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-              <h4 class="modal-title text-center">Bienvenue sur le site de gestion des salles d'attentes</h4>
-              <h5 class="text-center">Welcome to the waiting room manager of Montreal</h5>
-            </div>
-            <div class="modal-body">
+    <div id="shortModal" class="modal modal-wide fade">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+            <h4 class="modal-title text-center">Bienvenue sur le site de gestion des salles d'attentes</h4>
+            <h5 class="text-center">Welcome to the waiting room manager of Montreal</h5>
+          </div>
+          <div class="modal-body">
             <form class="col-lg-12" id="locateform">
               <div class="input-group input-group-lg col-sm-offset-3 col-sm-6">
                 <input type="text" class="center-block form-control input-lg" title="Entrez votre code postal." placeholder="Entrez votre code postal." id="address">
@@ -86,150 +86,150 @@ check_auth();
         </div><!-- /.modal-content -->
       </div><!-- /.modal-dialog -->
     </div><!-- /.modal -->
-     <script type="text/javascript">
-            var package_path = "//esri.github.io/bootstrap-map-js/src/js";
-            var dojoConfig = {
-                packages: [{
-                    name: "application",
-                    location: package_path
-                }]
-            };
-        </script>
-        <script src="https://js.arcgis.com/3.10compact"></script>
-        <script>
-            require(["esri/map", "application/bootstrapmap", "esri/dijit/LocateButton", "esri/tasks/locator", "esri/graphic", 
-                     "esri/symbols/SimpleMarkerSymbol", "esri/symbols/PictureMarkerSymbol", "esri/symbols/Font", "esri/symbols/TextSymbol", 
-                     "dojo/_base/array", "esri/Color", "dojo/number", "dojo/parser", "dojo/dom", "dijit/registry", "dojo/domReady!"],
+    <script type="text/javascript">
+      var package_path = "//esri.github.io/bootstrap-map-js/src/js";
+      var dojoConfig = {
+        packages: [{
+          name: "application",
+            location: package_path
+        }]
+      };
+    </script>
+    <script src="https://js.arcgis.com/3.10compact"></script>
+    <script>
+      require(["esri/map", "application/bootstrapmap", "esri/dijit/LocateButton", "esri/tasks/locator", "esri/graphic", 
+                "esri/symbols/SimpleMarkerSymbol", "esri/symbols/PictureMarkerSymbol", "esri/symbols/Font", "esri/symbols/TextSymbol", 
+                "dojo/_base/array", "esri/Color", "dojo/number", "dojo/parser", "dojo/dom", "dijit/registry", "dojo/domReady!"],
 
-              function(Map, BootstrapMap, LocateButton, Locator, Graphic, SimpleMarkerSymbol, PictureMarkerSymbol, Font, TextSymbol, arrayUtils, Color, number, parser, dom, registry) {
-                parser.parse();
-                // Get a reference to the ArcGIS Map class
-                var map = BootstrapMap.create("mapDiv",{
-                  basemap:"osm",
-                  center:[-73.5844, 45.5379],
-                  zoom:12
-                });
+      function(Map, BootstrapMap, LocateButton, Locator, Graphic, SimpleMarkerSymbol, PictureMarkerSymbol, Font, TextSymbol, arrayUtils, Color, number, parser, dom, registry) {
+        parser.parse();
+        // Get a reference to the ArcGIS Map class
+        var map = BootstrapMap.create("mapDiv",{
+          basemap:"osm",
+          center:[-73.5844, 45.5379],
+          zoom:12
+        });
 
-                geoLocate = new LocateButton({
-                  map: map
-                }, "LocateButton");
-                geoLocate.startup();
+        geoLocate = new LocateButton({
+          map: map
+        }, "LocateButton");
+        geoLocate.startup();
 
-                $.getJSON( "hopitaux.php", function( data ) {
-                  $.each( data, function( key, val ) {
-                    //alert(val['Nom']);
-                    var latLongPoint = new esri.geometry.Point(val['Longitude'], val['Latitude']);
-                    
-                    var symbol = new esri.symbol.SimpleMarkerSymbol().setSize(8).setColor(new dojo.Color([255, 0, 0]));
-                    var graphic = new esri.Graphic(latLongPoint, symbol);
-                    var infoTemplate = new esri.InfoTemplate();
-                    infoTemplate.setTitle(val['Nom']);
-                    var address = val['Adresse'];
-                    var hopital = val['ID'];
-                    
-                    $.getJSON( "departements.php?ID=" + val['ID'], function( data ) {
-                      var content = address + '<br><br>';
-                      
-                      $.each( data, function( key, val ) {
-                        content += '<button type="button" class="btn btn-default deptselect" onclick="showAttente(' + hopital + ', ' + val['ID'] + ');">' +  val['Nom'] + '</button>';
-                      });
-                      
-                      content += '<br><div id="attente_' + hopital + '">&nbsp;</div>';
-                      content += '<div id="updated_' + hopital + '">&nbsp;</div>';
-                      infoTemplate.setContent(content);
-                    });
-                    graphic.setInfoTemplate(infoTemplate);
-                    map.graphics.add(graphic);
-                  });
-               });
-
-               locator = new Locator("https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer");
-               locator.on("address-to-locations-complete", showResults);
-
-               $("locategps").click( function() {
-                 geoLocate._locate();
-                 $('#shortModal').modal('hide');
-               });
-
-               $("#locateform").submit( function() {
-                 event.preventDefault();
-                 var address = {
-                   "SingleLine": $("#address").val()
-                 };
-                 locator.outSpatialReference = map.spatialReference;
-                 var options = {
-                   address: address,
-                   outFields: ["Loc_name"]
-                 };
-                 locator.addressToLocations(options);
-                 $('#shortModal').modal('hide');
-               });
-          
-               function showResults(evt) {
-                 var symbol = new PictureMarkerSymbol('https://js.arcgis.com/3.10compact/js/esri/dijit/images/sdk_gps_location.png', 28, 28);
-                 var geom;
-                 //arrayUtils.every(evt.addresses, function(candidate) {
-                 $.each( evt.addresses, function(i, candidate) {
-                   console.log(candidate.score);
-                   if (candidate.score > 80) {
-                     console.log(candidate.location);
-                     var attributes = { 
-                       address: candidate.address, 
-                       score: candidate.score, 
-                       locatorName: candidate.attributes.Loc_name 
-                     };   
-                     geom = candidate.location;
-                     //var graphic = new Graphic(geom, symbol, attributes, infoTemplate);
-                     var graphic = new Graphic(geom, symbol, attributes);
-                     //add a graphic to the map at the geocoded location
-                     map.graphics.add(graphic);
-                     //add a text symbol to the map listing the location of the matched address.
-                     var displayText = candidate.address;
-                     var font = new Font(
-                       "16pt",
-                       Font.STYLE_NORMAL, 
-                       Font.VARIANT_NORMAL,
-                       Font.WEIGHT_BOLD,
-                       "Helvetica"
-                     );
-                    
-                     var textSymbol = new TextSymbol(
-                       displayText,
-                       font,
-                       new Color("#666633")
-                     );
-                     textSymbol.setOffset(0,8);
-                     map.graphics.add(new Graphic(geom, textSymbol));
-                     return false; //break out of loop after one candidate with score greater  than 80 is found.
-                   }
-                 });
-                 if ( geom !== undefined ) {
-                   map.centerAndZoom(geom, 12);
-                 }
-               }
-
-               $(".modal-wide").on("show.bs.modal", function() {
-                 var height = $(window).height() - 200;
-                 $(this).find(".modal-body").css("max-height", height);
-               });
-
-               $(window).load(function(){
-                 $('#shortModal').modal('show');
-               });
-            });
+        $.getJSON( "hopitaux.php", function( data ) {
+          $.each( data, function( key, val ) {
+            //alert(val['Nom']);
+            var latLongPoint = new esri.geometry.Point(val['Longitude'], val['Latitude']);
+               
+            var symbol = new esri.symbol.SimpleMarkerSymbol().setSize(8).setColor(new dojo.Color([255, 0, 0]));
+            var graphic = new esri.Graphic(latLongPoint, symbol);
+            var infoTemplate = new esri.InfoTemplate();
+            infoTemplate.setTitle(val['Nom']);
+            var address = val['Adresse'];
+            var hopital = val['ID'];
             
-            function showAttente(hopital, departement) {
-
-              $.getJSON( "attente.php?ID=" + departement, function( data ) {
-                var content = ""; 
-                $.each( data, function( key, val ) {
-                  content += '<div class="alert alert-' + val['Label'] + '">' + val['Description'] + '</div>';
-                });
-                $("#attente_" + hopital).html(content);
+            $.getJSON( "departements.php?ID=" + val['ID'], function( data ) {
+              var content = address + '<br><br>';
+                 
+              $.each( data, function( key, val ) {
+                content += '<button type="button" class="btn btn-default deptselect" onclick="showAttente(' + hopital + ', ' + val['ID'] + ');">' +  val['Nom'] + '</button>';
               });
-              $("#updated_" + hopital).html('<div class="well well-sm">Dernière mise à jour: ' + Date() + '</div>');
+              
+              content += '<br><div id="attente_' + hopital + '">&nbsp;</div>';
+              content += '<div id="updated_' + hopital + '">&nbsp;</div>';
+              infoTemplate.setContent(content);
+            });
+            graphic.setInfoTemplate(infoTemplate);
+            map.graphics.add(graphic);
+          });
+        });
+
+        locator = new Locator("https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer");
+        locator.on("address-to-locations-complete", showResults);
+
+        $("locategps").click( function() {
+          geoLocate._locate();
+          $('#shortModal').modal('hide');
+        });
+
+        $("#locateform").submit( function() {
+          event.preventDefault();
+          var address = {
+            "SingleLine": $("#address").val()
+          };
+          locator.outSpatialReference = map.spatialReference;
+          var options = {
+            address: address,
+            outFields: ["Loc_name"]
+          };
+          locator.addressToLocations(options);
+          $('#shortModal').modal('hide');
+        });
+
+        function showResults(evt) {
+          var symbol = new PictureMarkerSymbol('https://js.arcgis.com/3.10compact/js/esri/dijit/images/sdk_gps_location.png', 28, 28);
+          var geom;
+          //arrayUtils.every(evt.addresses, function(candidate) {
+          $.each( evt.addresses, function(i, candidate) {
+          console.log(candidate.score);
+            if (candidate.score > 80) {
+              console.log(candidate.location);
+                var attributes = { 
+                address: candidate.address, 
+                score: candidate.score, 
+                locatorName: candidate.attributes.Loc_name 
+              };   
+              geom = candidate.location;
+              //var graphic = new Graphic(geom, symbol, attributes, infoTemplate);
+              var graphic = new Graphic(geom, symbol, attributes);
+              //add a graphic to the map at the geocoded location
+              map.graphics.add(graphic);
+              //add a text symbol to the map listing the location of the matched address.
+              var displayText = candidate.address;
+              var font = new Font(
+                "16pt",
+                Font.STYLE_NORMAL, 
+                Font.VARIANT_NORMAL,
+                Font.WEIGHT_BOLD,
+                "Helvetica"
+              );
+               
+             var textSymbol = new TextSymbol(
+               displayText,
+               font,
+               new Color("#666633")
+             );
+             textSymbol.setOffset(0,8);
+             map.graphics.add(new Graphic(geom, textSymbol));
+             return false; //break out of loop after one candidate with score greater  than 80 is found.
             }
-        </script>
+          });
+
+          if ( geom !== undefined ) {
+            map.centerAndZoom(geom, 12);
+          }
+        }
+
+        $(".modal-wide").on("show.bs.modal", function() {
+          var height = $(window).height() - 200;
+          $(this).find(".modal-body").css("max-height", height);
+        });
+
+        $(window).load(function(){
+          $('#shortModal').modal('show');
+        });
+      });
+         
+      function showAttente(hopital, departement) {
+        $.getJSON( "attente.php?ID=" + departement, function( data ) {
+          var content = ""; 
+          $.each( data, function( key, val ) {
+            content += '<div class="alert alert-' + val['Label'] + '">' + val['Description'] + '</div>';
+          });
+          $("#attente_" + hopital).html(content);
+        });
+        $("#updated_" + hopital).html('<div class="well well-sm">Dernière mise à jour: ' + Date() + '</div>');
+      }
+    </script>
   
     <!-- Bootstrap core JavaScript
     ================================================== -->
