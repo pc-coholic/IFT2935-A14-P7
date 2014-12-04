27c10209a8360dbbeed8e07eb5cb333fd7e78b6b
Martin Gross <martin@pc-coholic.de>
2014-12-04 02:02:30 -0500

    new hopital query

--- hopitaux.php
+++ hopitaux.php
@@ -1,5 +1,5 @@
 <?php
 require("db.php");
 
-print query_json("SELECT * FROM hopital");
+print query_json("SELECT h.ID, NOM, adresse, latitude, longitude, AVG(DateService - DATEARRIVEE) AS MOYENNE from hopital h, PATIENT_DANS_DEPT P , DEPT_HOPITAL D where h.id=d.id_h and d.id=p.id_dept_hop and p.DateService is not null group by nom");
 ?>
