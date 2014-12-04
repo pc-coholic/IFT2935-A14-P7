a4406ea92dd60afc564c377e557951454279e42d
Martin Gross <martin@pc-coholic.de>
2014-12-04 01:02:52 -0500

    departement fix

--- departements.php
+++ departements.php
@@ -1,5 +1,5 @@
 <?php
 require("db.php");
 
-print query_json("SELECT d.ID, Nom, AVG(DateService - DATEARRIVEE) AS Moyenne  from departement de, PATIENT_DANS_DEPT P , DEPT_HOPITAL D  where de.id=d.id_d and d.id=p.id_dept_hop and d.id_h=" . $_GET['ID'] . " and p.DateService is not null group by NOM";
+print query_json("SELECT d.ID, Nom, AVG(DateService - DATEARRIVEE) AS Moyenne  from departement de, PATIENT_DANS_DEPT P , DEPT_HOPITAL D  where de.id=d.id_d and d.id=p.id_dept_hop and d.id_h=" . $_GET['ID'] . " and p.DateService is not null group by NOM");
 ?>
