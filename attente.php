1a049bd9481ba90ac4ad73bc1b2118df4a809941
Farah DHOUIB <dh.farah@gmail.com>
2014-12-04 06:03:47 -0500

    reglage de l'attente : probleme au niveau de la requete reglée

--- attente.php
+++ attente.php
@@ -4,7 +4,7 @@ require("db.php");
 $attente = query_json("SELECT Description As Severite_des_patients, COUNT(*) AS Attente, Label FROM SEVERITE S, EVALUER E, PATIENT_DANS_DEPT P, DEPT_HOPITAL D 
                  WHERE S.ID=E.ID_S AND E.NUMEROPATIENT=P.NUMEROPATIENT AND P.ID_DEPT_HOP = D.ID AND D.ID = " . $_GET['ID'] . " AND P.DATESERVICE IS NULL GROUP BY Severite_des_patients");
 
-$moyenne = query_json("SELECT Description As Severite_des_patients,AVG(abs(DateService - DATEARRIVEE)) AS MOYENNE FROM SEVERITE S, EVALUER E, PATIENT_DANS_DEPT P , DEPT_HOPITAL D , INFIRMIERE I WHERE S.ID=E.ID_S AND E.NUMEROPATIENT=P.NUMEROPATIENT AND P.ID_DEPT_HOP = D.ID AND D.ID = " . $_GET['ID'] . " AND P.DateService IS NOT NULL GROUP BY Severite_des_patients");
+$moyenne = query_json("SELECT Description As Severite_des_patients, SEC_TO_TIME(AVG(TIME_TO_SEC(TIMEDIFF(DateService,DATEARRIVEE)))) AS MOYENNE FROM SEVERITE S, EVALUER E, PATIENT_DANS_DEPT P , DEPT_HOPITAL D WHERE S.ID=E.ID_S AND E.NUMEROPATIENT=P.NUMEROPATIENT AND P.ID_DEPT_HOP = D.ID AND D.ID = " . $_GET['ID'] . " AND P.DateService IS NOT NULL GROUP BY Severite_des_patients");
 
 $attente = json_decode($attente, true);
 $moyenne = json_decode($moyenne, true);
