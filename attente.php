533dd9f3eb504d873bbf93002be07b8f27e5d2c4
Martin Gross <martin@pc-coholic.de>
2014-12-03 23:20:49 -0500

    fix time display

--- attente.php
+++ attente.php
@@ -10,15 +10,11 @@ $attente = json_decode($attente, true);
 $moyenne = json_decode($moyenne, true);
 
 for ($i = 0; $i < sizeof($attente); $i++) {
-  $temps = 1;
+  $temps = "0:01";
   for ($j = 0; $j < sizeof($moyenne); $j++) {
     if ($moyenne[$j]['Severite_des_patients'] == $attente[$i]['Severite_des_patients']) {
       $temps = substr($moyenne[$j]['MOYENNE'], 0, -2);
       $temps = strrev(wordwrap(strrev($temps), 2, ':', true));
-      if (strlen($temps) == 2) {
-        $temps = "0:" . $temps;
-      } elseif (strlen($temps) == 1) {
-        $temps = "0:0" . $temps;
       }
       break;
     }
