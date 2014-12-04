bff0b3d27249d644f81d15db820f6c7933c16ebb
Martin Gross <martin@pc-coholic.de>
2014-12-03 22:42:46 -0500

    Debug attente

--- attente.php
+++ attente.php
@@ -6,8 +6,8 @@ $attente = query_json("SELECT Description As Severite_des_patients, COUNT(*) AS 
 
 $moyenne = query_json("SELECT Description As Severite_des_patients,AVG(DateService - DATEARRIVEE) AS MOYENNE FROM SEVERITE S, EVALUER E, PATIENT_DANS_DEPT P , DEPT_HOPITAL D , INFIRMIERE I WHERE S.ID=E.ID_S AND E.NUMEROPATIENT=P.NUMEROPATIENT AND P.ID_DEPT_HOP = D.ID AND D.ID = " . $_GET['ID'] . " AND P.DateService IS NOT NULL GROUP BY Severite_des_patients;");
 
-$attente = json_decode($attente);
-$moyenne = json_decode($moyenne);
+$attente = json_decode($attente, true);
+$moyenne = json_decode($moyenne, true);
 
 print($attente[0]['Severite_des_patients']);
 ?>
