7bcaf991e95d41399bd8fa5133b1d159b41b7d15
Farah DHOUIB <dh.farah@gmail.com>
2014-12-04 06:46:18 -0500

    SEC_TO_TIME non necessaire ?

--- attente.php
+++ attente.php
@@ -4,7 +4,9 @@ require("db.php");
 $attente = query_json("SELECT Description As Severite_des_patients, COUNT(*) AS Attente, Label FROM SEVERITE S, EVALUER E, PATIENT_DANS_DEPT P, DEPT_HOPITAL D 
                  WHERE S.ID=E.ID_S AND E.NUMEROPATIENT=P.NUMEROPATIENT AND P.ID_DEPT_HOP = D.ID AND D.ID = " . $_GET['ID'] . " AND P.DATESERVICE IS NULL GROUP BY Severite_des_patients");
 
-$moyenne = query_json("SELECT Description As Severite_des_patients, SEC_TO_TIME(AVG(TIME_TO_SEC(TIMEDIFF(DateService,DATEARRIVEE)))) AS MOYENNE FROM SEVERITE S, EVALUER E, PATIENT_DANS_DEPT P , DEPT_HOPITAL D WHERE S.ID=E.ID_S AND E.NUMEROPATIENT=P.NUMEROPATIENT AND P.ID_DEPT_HOP = D.ID AND D.ID = " . $_GET['ID'] . " AND P.DateService IS NOT NULL GROUP BY Severite_des_patients");
+$moyenne = query_json("SELECT Description As Severite_des_patients, AVG(TIME_TO_SEC(TIMEDIFF(DateService,DATEARRIVEE))) AS MOYENNE FROM SEVERITE S, EVALUER E, PATIENT_DANS_DEPT P , DEPT_HOPITAL D WHERE S.ID=E.ID_S AND E.NUMEROPATIENT=P.NUMEROPATIENT AND P.ID_DEPT_HOP = D.ID AND D.ID = " . $_GET['ID'] . " AND P.DateService IS NOT NULL GROUP BY Severite_des_patients");
+
+// SEC TO TIME a mettre pour avoir le format en hh:mm:ss
 
 $attente = json_decode($attente, true);
 $moyenne = json_decode($moyenne, true);
