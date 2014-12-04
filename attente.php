ebce5865e67a1057b81b8197523973ec0eb438d0
Martin Gross <martin@pc-coholic.de>
2014-12-03 20:50:46 -0500

    First part of attente-provider

--- attente.php
+++ attente.php
@@ -1,6 +1,6 @@
 <?php
 require("db.php");
 
-print query_json("SELECT Description As Severite_des_patients, COUNT(*) AS Attente FROM SEVERITE S, EVALUER E, PATIENT_DANS_DEPT P, DEPT_HOPITAL D 
+print query_json("SELECT Description As Severite_des_patients, COUNT(*) AS Attente, Label FROM SEVERITE S, EVALUER E, PATIENT_DANS_DEPT P, DEPT_HOPITAL D 
                   WHERE S.ID=E.ID_S AND E.NUMEROPATIENT=P.NUMEROPATIENT AND P.ID_DEPT_HOP = D.ID AND D.ID = " . $_GET['ID'] . " AND P.DATESERVICE IS NULL GROUP BY Severite_des_patients");
 ?>
