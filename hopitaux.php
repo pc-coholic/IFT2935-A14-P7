5a29813339ed2e177bd821ba465ed69f4aba206c
Farah DHOUIB <dh.farah@gmail.com>
2014-12-04 15:48:04 -0500

    Correction Moyenne Generale :D

--- hopitaux.php
+++ hopitaux.php
@@ -1,5 +1,6 @@
 <?php
 require("db.php");
 
-print query_json("SELECT h.ID, Nom, Adresse, Latitude, Longitude, AVG(DateService - DATEARRIVEE) AS Moyenne from hopital h, PATIENT_DANS_DEPT P , DEPT_HOPITAL D where h.id=d.id_h and d.id=p.id_dept_hop and p.DateService is not null group by nom");
+print query_json("SELECT h.ID, Nom, Adresse, Latitude, Longitude, AVG(TIME_TO_SEC(TIMEDIFF(DateService,DATEARRIVEE))) AS Moyenne from hopital h, PATIENT_DANS_DEPT P , DEPT_HOPITAL D where h.id=d.id_h and d.id=p.id_dept_hop and p.DateService is not null group by nom");
 ?>
+
