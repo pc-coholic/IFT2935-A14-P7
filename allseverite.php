c014fb3933c23073f81f9aeee6df7fe9ef61b13b
Farah DHOUIB <dh.farah@gmail.com>
2014-12-04 21:37:13 -0500

    ajout color

--- allseverite.php
+++ allseverite.php
@@ -1,7 +1,7 @@
 <?php
 require("db.php");
 
-$allSeverite= query_json("SELECT s.id as severite, count(*) As NbPatient from severite s, evaluer e, patient_dans_dept p where s.id=e.id_s and e.numeropatient=p.numeropatient and dateservice is null group by s.id");
+$allSeverite= query_json("SELECT s.id as severite, Label, count(*) As NbPatient from severite s, evaluer e, patient_dans_dept p where s.id=e.id_s and e.numeropatient=p.numeropatient and dateservice is null group by s.id");
 
 $allSeverite= json_decode($allSeverite, true);
 
